4651c0f1bcf8a75588161821112cfd7d7c16dba0
puerchik <rumyantsevnn@yandex.ru>
2024-05-26 09:15:56 +0300

    project preparation

---
+++ src/app/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { UserSearch } from "@/pages/UserSearch";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <UserSearch />
+  </React.StrictMode>
+);

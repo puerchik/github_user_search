3da367302c046075d007dc7d73a09a7f3c17ba57
puerchik <rumyantsevnn@yandex.ru>
2024-05-27 08:54:39 +0300

    Updated dependencies, added react-hook-form, and refactored Search feature

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -1,7 +1,20 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Input = {
+  username: string;
+};
+
 export const Search = () => {
+  const { register, handleSubmit } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = (data) => console.log(data.username);
+
   return (
     <>
-      <input type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("username")} type="text" />
+        <button>Search</button>
+      </form>
     </>
   );
 };

75fa05fb94bdff6b2e6af6df734183bc1f29bcdd
puerchik <rumyantsevnn@yandex.ru>
2024-05-27 15:58:11 +0300

    Enhanced Redux Store and Improved User Search Functionality

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
+import { GitHubUser } from "../types/response";
 
 export const instance = axios.create({
   baseURL: "https://api.github.com/users",
 });
+
+export const getUser = () => instance.get<GitHubUser>("");

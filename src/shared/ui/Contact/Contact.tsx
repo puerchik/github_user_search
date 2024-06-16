9cadadefada23dd6cd50040b647de054032ec756
puerchik <rumyantsevnn@yandex.ru>
2024-06-16 11:27:40 +0300

    README file edits

--- src/shared/ui/Contact/Contact.tsx
+++ src/shared/ui/Contact/Contact.tsx
@@ -1,66 +1,75 @@
-import styled, { css } from "styled-components";
-import { FlexRow } from "@/shared/const/FlexRow";
+import styled, { css } from 'styled-components'
+import { FlexRow } from '@/shared/const/FlexRow'
 
 type Props = {
-  icon: string;
-  link: boolean;
-  value: string | null;
-};
+  icon: string
+  link: boolean
+  value: string | null
+}
 
 export const Contact = ({ icon, link, value }: Props) => {
   return (
     <>
-      <ContactWrapper $disabled={!!value} $columnGap="15px">
-        <img src={icon} alt="" />
+      <ContactWrapper
+        $disabled={!!value}
+        $columnGap="15px"
+      >
+        <img
+          src={icon}
+          alt=""
+        />
         {link && value ? (
-          <a href={value} target="_blank">
+          <a
+            href={value}
+            target="_blank"
+          >
             {value}
           </a>
         ) : (
-          <p>{value ? value : "Not Available"}</p>
+          <p>{value ? value : 'Not Available'}</p>
         )}
       </ContactWrapper>
     </>
-  );
-};
+  )
+}
 
 const ContactWrapper = styled(FlexRow)<{
-  $disabled: boolean;
+  $disabled: boolean
 }>`
   justify-content: flex-start;
 
-${(props) =>
-  props.theme.theme === "dark"
-    ? `
+  ${props =>
+    props.theme.theme === 'dark'
+      ? `
   & a {
     color: #ffffff;
   }
   `
-    : `
+      : `
   & a {
     color: #333333;
   }
   `}
 
-& a {
-  transition: 0.2s;
+  & a {
+    transition: 0.2s;
 
-  &:hover {
-   color: ${(props) =>
-     props.theme.theme === "dark"
-       ? `#0177fb          
+    &:hover {
+      color: ${props =>
+        props.theme.theme === 'dark'
+          ? `#0177fb          
           `
-       : `
+          : `
         #ffffff;`};
+    }
   }
-}
 
   & img {
     height: 20px;
-    ${(props) => props.theme.theme === "light" && "filter: invert(1);"}    
+    ${props => props.theme.theme === 'light' && 'filter: invert(1);'}
   }
 
-  ${(props) =>
+  ${props =>
     !props.$disabled &&
     css`
       color: #808080;
@@ -69,5 +78,5 @@ ${(props) =>
       & img {
         opacity: 0.5;
       }
-    `}}
-`;
+    `}
+`
